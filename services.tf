b7c3a76a48abd4017c6938c0f0a49b79da509703
duyphuong <you@example.com>
2022-05-24 23:39:44 +0700

    fix: update data

--- services.tf
+++ services.tf
@@ -1,6 +1,6 @@
 provider "helm" {
   kubernetes {
-  host                   = "https://${google_container_cluster.primary.endpoint}"
+  host                   = "https://${data.google_container_cluster.primary.endpoint}"
   token                  = "${data.google_client_config.default.access_token}"
   cluster_ca_certificate = "${base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)}"
   }
